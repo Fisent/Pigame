7c69d01f7f92ae63798b6910cf060aeacb66dfd5
Fisent <lukzieniewicz@gmail.com>
2016-09-21 02:51:03 +0200

    Ads working

--- app/src/main/java/com/luzkzieniewicz/gmail/pigame/MainActivity.java
+++ app/src/main/java/com/luzkzieniewicz/gmail/pigame/MainActivity.java
@@ -3,6 +3,9 @@ package com.luzkzieniewicz.gmail.pigame;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
+import com.google.android.gms.ads.MobileAds;
 import com.google.firebase.analytics.FirebaseAnalytics;
 
 public class MainActivity extends AppCompatActivity
@@ -14,5 +17,13 @@ public class MainActivity extends AppCompatActivity
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //change the number when app will be added
+        MobileAds.initialize(getApplicationContext(), "ca-app-pub-3940256099942544~3347511713");
+
+        AdView mAdView = (AdView) findViewById(R.id.adView);
+        AdRequest adRequest = new AdRequest.Builder().build();
+        mAdView.loadAd(adRequest);
+
     }
 }
